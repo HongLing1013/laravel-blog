de5f7cb79805a754dd8cdd41df62cab2eedb15ce
Hollie Chao <tsubasababy1013@gmail.com>
2023-04-04 14:18:06 +0800

    新增路由

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'index';
+});
+
+Route::get('/about', function () {
+    return 'about';
+});
+
+Route::get('/content', function () {
+    return 'content';
+});
+
+Route::get('/posts', function () {
+    return 'posts.list';
+});
+
+Route::get('/posts/{id}', function ($id) {
+    return 'posts.show';
 });
